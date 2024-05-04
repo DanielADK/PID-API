e4b71cee497cdfa9b429fbdf5880799976f2315f
Daniel Adámek <daniel.adamek123@icloud.com>
2024-05-04 18:44:58 +0200

    schema, readme, phpstan configuration, compose remove

---
+++ README.md
@@ -0,0 +1,26 @@
+# PID API
+
+## Description
+The PID API is a web service that provides access to data about stores and their opening hours. The API is built on Symfony and uses SQL to store and retrieve data.
+
+## Endpoints
+
+### `/api/point/fetch`
+
+- Method: `GET`
+- Description: Fetches a list of stores into the database.
+- Parameters: None
+- Example of use: `curl http://localhost/api/point/fetch`
+
+### `/api/point/opened`
+
+- Method: `GET`
+- Description: Returns a list of stores that are open at a given time and date.
+- Parameters:
+    - `time`: Time in the format `HH:MM`. If not set, the current time is used.
+    - `date`: Date in the format `YYYY-MM-DD`. If not set, the current date is used.
+- Example use: `curl "http://localhost/api/point/opened?time=10:00&date=2022-12-31"`
+
+# SQL Schema
+The SQL schema is defined in the file `schema.sql`. 
+![Database Schema](schema.png)
---
+++ schema.sql
@@ -0,0 +1,51 @@
+create table if not exists opening_hours
+(
+    id        int auto_increment
+        constraint `PRIMARY`
+        primary key,
+    day_from  int  not null,
+    day_to    int  not null,
+    time_from time not null,
+    time_to   time not null,
+    constraint openinghoursIdx
+        unique (day_from, day_to, time_from, time_to)
+)
+    collate = utf8mb4_unicode_ci;
+
+create table if not exists point_of_sale
+(
+    id          varchar(255) not null
+        constraint `PRIMARY`
+        primary key,
+    type        varchar(255) not null,
+    name        varchar(255) not null,
+    address     varchar(255) null,
+    latitude    double       not null,
+    longitude   double       not null,
+    services    int          not null,
+    pay_methods int          not null,
+    link        varchar(255) null
+)
+    collate = utf8mb4_unicode_ci;
+
+create table if not exists point_of_sale_opening_hours
+(
+    point_of_sale_id varchar(255) not null,
+    opening_hours_id int          not null,
+    constraint `PRIMARY`
+        primary key (point_of_sale_id, opening_hours_id),
+    constraint FK_E42839DC6B7E9A73
+        foreign key (point_of_sale_id) references point_of_sale (id)
+            on delete cascade,
+    constraint FK_E42839DCCE298D68
+        foreign key (opening_hours_id) references opening_hours (id)
+            on delete cascade
+)
+    collate = utf8mb4_unicode_ci;
+
+create index IDX_E42839DC6B7E9A73
+    on point_of_sale_opening_hours (point_of_sale_id);
+
+create index IDX_E42839DCCE298D68
+    on point_of_sale_opening_hours (opening_hours_id);
+
